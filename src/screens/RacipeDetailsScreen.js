c284b3f0b1250082ff8270d2a634bbd1f92908e1
Muhammed Niyad <niyadpunniyath007@gmail.com>
2024-05-09 08:49:58 +0530

    racipe details page started to build

--- src/screens/RacipeDetailsScreen.js
+++ src/screens/RacipeDetailsScreen.js
@@ -1,11 +1,74 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { CachedImage } from '../helpers/image';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import {ChevronLeftIcon} from 'react-native-heroicons/outline'
+import {HeartIcon} from 'react-native-heroicons/solid'
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import { Loading } from '../components/loading';
 
-const RacipeDetailsScreen = () => {
+const RacipeDetailsScreen = (props) => {
+  let item = props.route.params;
+  const [isFavourite, setIsFavourite]= useState(false)
+  const navigation = useNavigation();
+  const [recipeDetails,setRacipedetails] = useState(null);
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(()=>{
+    getRecipeDetails(item.idMeal)
+  },[])
+  
+  const getRecipeDetails = async (id)=>{
+    try {
+      const response = await axios.get(`https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      // console.log('Categories Data:', response.data);
+      if(response && response.data){
+        setRacipedetails(response.data.meals[0]);
+        setLoading(false)
+      }
+    } catch (error) {
+      console.log('error: ', error.message);
+    }
+  }
+
   return (
-    <View>
-      <Text>RacipeDetailsScreen</Text>
-    </View>
+    <ScrollView
+    className="bg-white flex-1"
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={{paddingBottom:30}}>
+      <StatusBar style='light'/>
+      {/* racipe image */}
+      <View className='flex-row justify-center'>
+        <CachedImage 
+        uri={item.strMealThumb}
+        style={{width: wp(98), height:hp(50), borderRadius:40,}} className={"mt-1"}/>
+      </View>
+      {/* back button */}
+      <View className='w-full absolute flex-row justify-between items-center pt-14'>
+        <TouchableOpacity onPress={()=>navigation.goBack()} className='py-2 pl-2 pr-[10px] rounded-full ml-5 bg-white'>
+          <ChevronLeftIcon size={hp(3.1)} strokeWidth={4.5} color={"#fbbf24"}/>
+        </TouchableOpacity>
+        <TouchableOpacity className='p-2 rounded-full mr-5 bg-white' onPress={()=>setIsFavourite(!isFavourite)}>
+          <HeartIcon size={hp(3.1)} strokeWidth={4.5} color={isFavourite? "red" : "gray"}/>
+        </TouchableOpacity>
+      </View>
+
+      {/* racipe description */}
+      {
+        loading?(
+          <Loading size="large" className='mt-16' color='gray' />
+        ) : (
+          <View>
+            <Text>discription</Text>
+          </View>
+        )
+      }
+    </ScrollView>
   )
 }
 
